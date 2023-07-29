216240315a567c7a7f5221344c6a87362b435e58
AndresSebad <asebastian@uc.cl>
2023-07-29 02:50:28 -0400

    first commit

---
+++ emo_utils.py
@@ -0,0 +1,32 @@
+import csv
+import numpy as np
+import emoji
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
+def read_csv(filename = 'data/emojify_data.csv'):
+    phrase = []
+    emoji = []
+
+    with open (filename) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+
+        for row in csvReader:
+            phrase.append(row[0])
+            emoji.append(row[1])
+
+    X = np.asarray(phrase)
+    Y = np.asarray(emoji, dtype=int)
+
+    return X, Y
+
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)]
+    return Y
